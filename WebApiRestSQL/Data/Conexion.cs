94d16825aad598ff7f63e38f068c85c0971a7e1b
Arturo Martinez <arturo.martinez@corebts.com>
2026-01-11 17:46:37 -0500

    Add project files.

---
+++ WebApiRestSQL/Data/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiRestSQL.Data
+{
+    public class Conexion
+    {
+        public static string rutaConexion = "Data Source=CORE-5CD3362RVW;Initial Catalog=DBAPIRESTSQL;Integrated Security=True";
+    }
+}
